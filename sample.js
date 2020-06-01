c52c3bb65cde4ce1c8b58cc705ef8963a10226b4
kch <wilzokch@gmail.com>
2020-06-01 12:14:11 +0800

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,39 @@
+pm.globals.set("$randomStateId", randFixNumberByRange(1, 14));
+pm.globals.set("$randomGender", randChoice('M/F'));
+pm.globals.set("$randomPostCode", randFixNumber(5));
+
+// function declaration
+function randNumber(length) {
+    return randRound(Math.pow(10,length));
+}
+
+function randRound(length) {
+    return Math.round(Math.random() * length);
+}
+
+function randFixNumber(length) {
+    return pad(randNumber(length), length);
+}
+
+function randNumberByRange(min, max) {
+    return min + randRound(max-min);
+}
+
+function randFixNumberByRange(min, max) {
+    var value = "" + max;
+    var rand = randNumberByRange(min, max);
+
+    return pad(rand, value.length);
+}
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+function randChoice(text) {
+    var options = text.split('/');
+    var index = randFixNumberByRange(0, options.length -1);
+    return options[index];
+}
